bfa022705619fd2205a1821799ef8468e015f4b5
Raditya <Radityaega@student.telkomuniversity.ac.id>
2023-05-20 03:28:14 +0700

    Initial commit

--- app/src/main/java/org/d3if0103/assessment/db/ConverterEntity.kt
+++ app/src/main/java/org/d3if0103/assessment/db/ConverterEntity.kt
@@ -3,7 +3,7 @@ package org.d3if0103.assessment.db
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "converterpanjang")
 data class ConverterEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
